00b3651f3b216e4e4d431aedece01fd4c684e2f6
Svenja Pape <Svenja.Pape@ewetel.net>
2015-06-08 19:17:43 +0200

    Signed-off-by: Svenja Pape <Svenja.Pape@ewetel.net> Version 1.0 von der Wellenausbreitung. NOCH KEINE RICHTIGE GUI Funktionen: - Überlagerung der Sinuswellen - Einstellen der Amplitude, Frequenzen und Phase

---
+++ SinusWaves1.m
@@ -0,0 +1,85 @@
+clear all
+close all
+
+%Quelle:
+%-Ansicht von oben: https://nf.nci.org.au/facilities/software/Matlab/techdoc/ref/pcolor.html 
+c = 343;          % sound velocity for calculating frequency
+
+
+%% Ideen:
+% - Vielleicht noch einen Dämpfungsfaktor?
+% - 
+
+%% Input dialog for disired parameters of sources
+prompt={'Enter frequency of source A:',...
+        'Enter frequency of source B:',...
+        'Enter amplitude of source A:', ...
+        'Enter amplitude of source B:', ...
+        'Enter phase of source A:'...
+        'Enter phase of source B:', ...
+        '3D oder 2D eingeben'};
+name='Input for sinusoids';
+numlines=1;
+defaultanswer={'100','10', '1', '1',  '0', '180', '2D'};
+answer=inputdlg(prompt,name,numlines,defaultanswer);
+
+freqA = str2double(answer{1});
+freqB = str2double(answer{2});
+amplA = str2double(answer{3});
+amplB = str2double(answer{4});
+phaseA = str2double(answer{5});
+phaseB = str2double(answer{6});
+angle = answer{7};
+
+
+%% Calculation of parameters used in generating sinusoids
+% input frequency is transformed to wave number
+kA = (2*pi()*freqA)/c;  
+kB = (2*pi()*freqB)/c;
+
+% input phase in degree is transformed to radiant
+phaseA = (2*pi()/360)*phaseA;
+phaseB = (2*pi()/360)*phaseB;
+
+hfig = figure();
+
+xa=-11:0.1:39;
+ya=-11:0.1:39;
+[Xa,Ya] = meshgrid(xa,ya); % greate rectangullar mesh
+
+xb=-20:0.1:30;
+yb=-20:0.1:30;
+[Xb,Yb] = meshgrid(xb,yb);
+
+xgrund = -25:0.1:25;
+ygrund = -25:0.1:25;
+[Xgrund,Ygrund] = meshgrid(xgrund,ygrund);
+
+
+Ra=sqrt(Xa.^2+Ya.^2);       %radius
+Rb=sqrt(Xb.^2+Yb.^2);       
+
+%% Animation of the sinusoidal waves
+for phi = linspace(30*pi(),0,500)
+
+Za = amplA * sin(kA * Ra + (phi+phaseA));
+Zb = amplB * sin(kB * Rb + (phi+phaseB));
+Zmix = Za + Zb;
+
+
+%surf(Xgrund,Ygrund, Z2);
+surf(Xgrund, Ygrund, Zmix);  %RICHTIGE ÜBERLAGERUNG
+
+if strcmp(angle,'3D')
+    view([0 50])
+else
+    view([0 90])
+end
+
+%axis equal;
+colormap('gray');
+shading interp;
+
+pause(0.05)
+
+end
